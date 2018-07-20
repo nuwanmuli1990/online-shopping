074d248e9c8a7599b09a69f64502070d1cf8857d
Mulitha Nuwan Kalansooriya <mulithanuwan@gmail.com>
2018-07-20 20:40:26 +0530

    Successfully fixed footer issue and apply a theme

--- onlineshopping/src/main/java/com/muli/java/controller/Home.java
+++ onlineshopping/src/main/java/com/muli/java/controller/Home.java
@@ -1,5 +1,7 @@
 package com.muli.java.controller;
 
+
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -7,10 +9,49 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class Home {
 
-	@RequestMapping(value= {"/", "/index", "/home"})
+	@RequestMapping(value= {"/", "/index", "/Home"})
 	public ModelAndView home() {
-		ModelAndView mv = new ModelAndView("home");
+		ModelAndView mv = new ModelAndView("index");
 		mv.addObject("greeting", "Welcome to MVC ");
+		mv.addObject("isHomePage", true);
 		return mv;
 	}
+	
+	@RequestMapping(value="/About")
+	public ModelAndView aboutUs() {
+		ModelAndView mv = new ModelAndView("index");
+		mv.addObject("greeting", "Welcome to MVC ");
+		mv.addObject("isAboutPage", true);
+		return mv;
+	}
+	
+	
+	@RequestMapping(value="/Contact")
+	public ModelAndView contactUs() {
+		ModelAndView mv = new ModelAndView("index");
+		mv.addObject("greeting", "Welcome to MVC ");
+		mv.addObject("isContactPage", true);
+		return mv;
+	}
+	
+//	@RequestMapping(value="/Test/{name}/{age}")
+//	public ModelAndView test(@PathVariable("name")String name, @PathVariable("age")int age) {
+//		if(name== null) {
+//			name = "Enter name please";
+//		}
+//		ModelAndView mv = new ModelAndView("home");
+//		mv.addObject("name", name);
+//		mv.addObject("age", age);
+//		return mv;
+//	}
+//	
+//	@RequestMapping(value="/Param")
+//	public ModelAndView Param(@RequestParam(value="name") String name, @RequestParam(value="age")int age) {
+//		ModelAndView mv = new ModelAndView("home");
+//		mv.addObject("name", name);
+//		mv.addObject("age", age);
+//		return mv;
+//	}
+	
+	
 }
